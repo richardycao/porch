3536695b71c8b714d00aaac8187ccd0063dea192
Richard Cao <richardcao@richards-mbp.lan>
2023-03-29 13:51:40 -0700

    test hellow world

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='porch',
+    version='0.0.1',
+    author='Richard Cao',
+    author_email='richardycao@gmail.com',
+    url='https://github.com/richardycao/porch.git',
+    packages=find_packages()
+)
